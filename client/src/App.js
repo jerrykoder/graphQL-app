04b7bfa69fffacc962b9b7f52b0a81b7c15cb590
Luis-Gerardo <webdesigner@gerardobarroso.com>
2019-04-18 16:45:00 -0700

    React Starter Template

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import Customers from './Components/Customers'
+
+const client = new ApolloClient({
+	uri: 'localhost:4000/graphql'
+})
+
+class App extends Component {
+	render() {
+		return (
+			<ApolloProvider>
+				<div className='App' client={client}>
+					<header className='App-header'>
+						<img src={logo} className='App-logo' alt='logo' />
+						<p>
+							Edit <code>src/App.js</code> and save to reload.
+						</p>
+						<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+							Learn React
+						</a>
+						<Customers />
+					</header>
+				</div>
+			</ApolloProvider>
+		)
+	}
+}
+
+export default App
