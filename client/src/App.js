56f150d2c727ba31141ef7f56472e3210fdab856
Luis-Gerardo <webdesigner@gerardobarroso.com>
2019-04-19 15:27:54 -0700

    Add query and create component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
@@ -14,16 +13,7 @@ class App extends Component {
 		return (
 			<ApolloProvider client={client}>
 				<div className='App'>
-					<header className='App-header'>
-						<img src={logo} className='App-logo' alt='logo' />
-						<p>
-							Edit <code>src/App.js</code> and save to reload.
-						</p>
-						<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-							Learn React
-						</a>
-						<Customers />
-					</header>
+					<Customers />
 				</div>
 			</ApolloProvider>
 		)
--- client/src/Components/Customers.js
+++ client/src/Components/Customers.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
-import gql from "graphql-tag";
+import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 
 const CUSTOMERS_QUERY = gql`
-
-	query CustomersQuery{
-	  customers {
-	    id
-	    name
-	    email
-	  }
+	query CustomersQuery {
+		customers {
+			id
+			name
+			email
+			post {
+				post_title
+				post_date
+			}
+		}
 	}
-
-`;
+`
 
 export default class Customers extends Component {
 	render() {
@@ -20,22 +22,20 @@ export default class Customers extends Component {
 			<div>
 				<h1>Customers</h1>
 				<Query query={CUSTOMERS_QUERY}>
-				
-					{
-						({ loading, error, data }) => {
-					      if (loading) return <p>Loading...</p>;
-					      if (error) return <p>Error :(</p>;
+					{({ loading, error, data }) => {
+						if (loading) return <p>Loading...</p>
+						if (error) return <p>Error :(</p>
 
-					      	console.log(data)
+						console.log(data)
 
-					      return data.customers.map(({ id, name, email }) => (
-					        <div key={id}>
-					          <p>{name}: {email}</p>
-					        </div>
-					      ));
-					    }
-					}
-
+						return data.customers.map(({ id, name, email }) => (
+							<div key={id}>
+								<p>
+									{name}: {email}
+								</p>
+							</div>
+						))
+					}}
 				</Query>
 			</div>
 		)
